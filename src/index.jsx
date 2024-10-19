c9fff7482e95c15bf2b368950294bb9759558b95
KyleBuii <buikyle12@gmail.com>
2024-10-19 14:10:40 -0400

    ### Quick Fix - Gave live2d-widget a null check

--- src/index.jsx
+++ src/index.jsx
@@ -3042,23 +3042,25 @@ class Widgets extends Component{
         }, { once: true });
         /// Modify live2d-widget
         let elementWaifuToggle = document.getElementById("waifu-toggle");
-        elementWaifuToggle.innerHTML = "SHOW";
-        if(document.getElementById("waifu-tool") === null){
-            elementWaifuToggle.click();
+        if(elementWaifuToggle !== null){
+            elementWaifuToggle.innerHTML = "SHOW";
+            if(document.getElementById("waifu-tool") === null){
+                elementWaifuToggle.click();
+            };
+            let elementLive2DMove = document.createElement("span");
+            elementLive2DMove.id = "waifu-tool-switch-side";
+            elementLive2DMove.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM297 385c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l71-71L120 280c-13.3 0-24-10.7-24-24s10.7-24 24-24l214.1 0-71-71c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L409 239c9.4 9.4 9.4 24.6 0 33.9L297 385z"/>
+                </svg>
+            `;
+            elementLive2DMove.onclick = () => {
+                let elementWaifu = document.getElementById("waifu");
+                elementWaifu.classList.toggle("waifu-right-side");
+            };
+            document.getElementById("waifu-tool")
+                .prepend(elementLive2DMove);
         };
-        let elementLive2DMove = document.createElement("span");
-        elementLive2DMove.id = "waifu-tool-switch-side";
-        elementLive2DMove.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                <path d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM297 385c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l71-71L120 280c-13.3 0-24-10.7-24-24s10.7-24 24-24l214.1 0-71-71c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L409 239c9.4 9.4 9.4 24.6 0 33.9L297 385z"/>
-            </svg>
-        `;
-        elementLive2DMove.onclick = () => {
-            let elementWaifu = document.getElementById("waifu");
-            elementWaifu.classList.toggle("waifu-right-side");
-        };
-        document.getElementById("waifu-tool")
-            .prepend(elementLive2DMove);
     };
     componentWillUnmount(){
         window.removeEventListener("beforeunload", this.storeData);
